f6ab7217b919bee6d773ca61bfcf8d6f3e7d1588
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-06 10:50:49 -0300

    feat: dismissable welcome banner

--- src/sections/dashboard/default/WelcomeBanner.js
+++ src/sections/dashboard/default/WelcomeBanner.js
@@ -5,6 +5,7 @@ import { useTheme } from '@mui/material/styles';
 // project import
 import MainCard from 'components/MainCard';
 import { ThemeDirection } from 'config';
+import useLocalStorage from 'hooks/useLocalStorage';
 
 //asset
 const WelcomeImage = '/assets/images/analytics/welcome-banner.png';
@@ -14,16 +15,19 @@ const WelcomeImageArrow = '/assets/images/analytics/welcome-arrow.png';
 
 const WelcomeBanner = () => {
   const theme = useTheme();
+  const [dismissed, setDismissed] = useLocalStorage('welcomeBanner', false);
 
   return (
     <MainCard
       border={false}
       sx={{
+        display: dismissed ? 'none' : 'block',
         background:
           theme.direction === ThemeDirection.RTL
             ? `linear-gradient(60.38deg, ${theme.palette.primary.lighter} 114%, ${theme.palette.primary.light} 34.42%, ${theme.palette.primary.main} 60.95%, ${theme.palette.primary.dark} 84.83%, ${theme.palette.primary.darker} 104.37%)`
             : `linear-gradient(250.38deg, ${theme.palette.primary.lighter} 2.39%, ${theme.palette.primary.light} 34.42%, ${theme.palette.primary.main} 60.95%, ${theme.palette.primary.dark} 84.83%, ${theme.palette.primary.darker} 104.37%)`
       }}
+
     >
       <Grid container>
         <Grid item md={6} sm={6} xs={12}>
@@ -34,15 +38,23 @@ const WelcomeBanner = () => {
             <Typography variant="h6" color={theme.palette.background.paper}>
               Acompanhe o desenvolvimento de suas colmeias
             </Typography>
-            {/* <Box>
+            <Box>
               <Button
                 variant="outlined"
                 color="secondary"
-                sx={{ color: theme.palette.background.paper, borderColor: theme.palette.background.paper }}
+                sx={{
+                  color: theme.palette.background.paper,
+                  borderColor: theme.palette.background.paper,
+                  '&:hover': {
+                    borderColor: theme.palette.primary.lighter,
+                    color: theme.palette.primary.lighter,
+                  }
+                }}
+                onClick={() => setDismissed(true)}
               >
-                View full statistic
+                Entendido
               </Button>
-            </Box> */}
+            </Box>
           </Stack>
         </Grid>
         <Grid item sm={6} xs={12} sx={{ display: { xs: 'none', sm: 'initial' } }}>
