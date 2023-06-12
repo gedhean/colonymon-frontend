4e8a68ee494eddbe0ab36633c6c00b5f3e732127
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-12 15:12:09 -0300

    feat: locale select

--- src/layout/MainLayout/Header/HeaderContent/index.js
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -1,17 +1,15 @@
 import { useMemo } from 'react';
 
 // material-ui
-
 import { Box, useMediaQuery } from '@mui/material';
 
 // project import
 import Search from './Search';
-import Message from './Message';
 import Profile from './Profile';
-import Notification from './Notification';
+// import Notification from './Notification';
 import MobileSection from './MobileSection';
-import MegaMenuSection from './MegaMenuSection';
 import DrawerHeader from 'layout/MainLayout/Drawer/DrawerHeader';
+import Localization from './Localization';
 
 import { MenuOrientation } from 'config';
 import useConfig from 'hooks/useConfig';
@@ -19,22 +17,23 @@ import useConfig from 'hooks/useConfig';
 // ==============================|| HEADER - CONTENT ||============================== //
 
 const HeaderContent = () => {
-  const { menuOrientation } = useConfig();
+  const { i18n, menuOrientation } = useConfig();
 
   const downLG = useMediaQuery((theme) => theme.breakpoints.down('lg'));
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const megaMenu = useMemo(() => <MegaMenuSection />, []);
+  // eslint-disable-next-line
+  const localization = useMemo(() => <Localization />, [i18n]);
 
   return (
     <>
-      {menuOrientation === MenuOrientation.HORIZONTAL && !downLG && <DrawerHeader open={true} />}
+      {menuOrientation === MenuOrientation.HORIZONTAL && !downLG && (
+        <DrawerHeader open={true} />
+      )}
       {!downLG && <Search />}
-      {/* {!downLG && megaMenu} */}
+      {!downLG && localization}
       {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
 
       {/* <Notification /> */}
-      {/* <Message /> */}
       {!downLG && <Profile />}
       {downLG && <MobileSection />}
     </>
