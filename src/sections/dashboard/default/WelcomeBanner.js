84a58aaeb85144ac36d72214097774005306765f
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-16 09:43:29 -0300

    feat: added more translations

--- src/sections/dashboard/default/WelcomeBanner.js
+++ src/sections/dashboard/default/WelcomeBanner.js
@@ -1,6 +1,9 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
 // material-ui
-import { Grid, Typography, Button, Stack, Box, CardMedia } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { Grid, Typography, Button, Stack, Box, CardMedia } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
@@ -27,16 +30,15 @@ const WelcomeBanner = () => {
             ? `linear-gradient(60.38deg, ${theme.palette.primary.lighter} 114%, ${theme.palette.primary.light} 34.42%, ${theme.palette.primary.main} 60.95%, ${theme.palette.primary.dark} 84.83%, ${theme.palette.primary.darker} 104.37%)`
             : `linear-gradient(250.38deg, ${theme.palette.primary.lighter} 2.39%, ${theme.palette.primary.light} 34.42%, ${theme.palette.primary.main} 60.95%, ${theme.palette.primary.dark} 84.83%, ${theme.palette.primary.darker} 104.37%)`
       }}
-
     >
       <Grid container>
         <Grid item md={6} sm={6} xs={12}>
           <Stack spacing={2} sx={{ padding: 3.4 }}>
             <Typography variant="h2" color={theme.palette.background.paper}>
-              Bem-vindo ao Colonymon
+              <FormattedMessage id="welcome-banner-tile" />
             </Typography>
             <Typography variant="h6" color={theme.palette.background.paper}>
-              Acompanhe o desenvolvimento de suas colmeias
+              <FormattedMessage id="welcome-banner-subtitle" />
             </Typography>
             <Box>
               <Button
@@ -47,21 +49,39 @@ const WelcomeBanner = () => {
                   borderColor: theme.palette.background.paper,
                   '&:hover': {
                     borderColor: theme.palette.primary.lighter,
-                    color: theme.palette.primary.lighter,
+                    color: theme.palette.primary.lighter
                   }
                 }}
                 onClick={() => setDismissed(true)}
               >
-                Entendido
+                <FormattedMessage id="welcome-banner-button" />
               </Button>
             </Box>
           </Stack>
         </Grid>
-        <Grid item sm={6} xs={12} sx={{ display: { xs: 'none', sm: 'initial' } }}>
-          <Stack sx={{ position: 'relative', pr: { sm: 3, md: 8 } }} justifyContent="center" alignItems="flex-end">
-            <CardMedia sx={{ width: 'inherit' }} component="img" src={WelcomeImage} alt="Welcome" />
+        <Grid
+          item
+          sm={6}
+          xs={12}
+          sx={{ display: { xs: 'none', sm: 'initial' } }}
+        >
+          <Stack
+            sx={{ position: 'relative', pr: { sm: 3, md: 8 } }}
+            justifyContent="center"
+            alignItems="flex-end"
+          >
+            <CardMedia
+              sx={{ width: 'inherit' }}
+              component="img"
+              src={WelcomeImage}
+              alt="Welcome"
+            />
             <Box sx={{ position: 'absolute', bottom: 0, right: '10%' }}>
-              <CardMedia component="img" src={WelcomeImageArrow} alt="Welcome Arrow" />
+              <CardMedia
+                component="img"
+                src={WelcomeImageArrow}
+                alt="Welcome Arrow"
+              />
             </Box>
           </Stack>
         </Grid>
