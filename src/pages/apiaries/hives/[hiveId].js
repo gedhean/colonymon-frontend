023c8743613c82a8499dec1d29e2facc08523660
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-26 21:37:14 -0300

    feat: hive page with id param

--- src/pages/apiaries/hives/[hiveId].js
+++ src/pages/apiaries/hives/[hiveId].js
@@ -1,6 +1,9 @@
 // material-ui
 import { Grid } from '@mui/material';
 
+// nextjs
+import { useRouter } from 'next/router';
+
 // project imports
 import Layout from 'layout';
 import Page from 'components/Page';
@@ -9,17 +12,23 @@ import HiveTable from 'sections/hives/HiveTable';
 
 // / ==============================|| Apiarios ||==============================
 
-const Hives = () => (
-  <Page title="Colméias">
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <MainCard title="Colméias" content={false}>
-          <HiveTable />
-        </MainCard>
+const Hives = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <Page title="Colméias">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <MainCard title="Colméias" content={false}>
+            <HiveTable />
+          </MainCard>
+        </Grid>
       </Grid>
-    </Grid>
-  </Page>
-);
+    </Page>
+  );
+};
 
 Hives.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
