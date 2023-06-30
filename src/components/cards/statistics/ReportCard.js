2b061a710ac40376801b2d2b049450d8e1c429b5
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-30 09:15:24 -0300

    feat: gain indicator

--- src/components/cards/statistics/ReportCard.js
+++ src/components/cards/statistics/ReportCard.js
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types';
 
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Chip, Grid, Stack, Typography } from '@mui/material';
 
 // project imports
 import MainCard from 'components/MainCard';
 
+// assets
+import { RiseOutlined, FallOutlined } from '@ant-design/icons';
+
 // ==============================|| REPORT CARD ||============================== //
 
-const ReportCard = ({ primary, secondary, iconPrimary, color }) => {
+const ReportCard = ({
+  primary,
+  secondary,
+  iconPrimary,
+  color,
+  percentage,
+  isLoss
+}) => {
   const IconPrimary = iconPrimary;
   const primaryIcon = iconPrimary ? <IconPrimary fontSize="large" /> : null;
 
@@ -17,7 +27,38 @@ const ReportCard = ({ primary, secondary, iconPrimary, color }) => {
       <Grid container justifyContent="space-between" alignItems="center">
         <Grid item>
           <Stack spacing={1}>
-            <Typography variant="h4">{primary}</Typography>
+            <Grid container alignItems="center">
+              <Grid item>
+                <Typography variant="h4" color="inherit">
+                  {primary}
+                </Typography>
+              </Grid>
+              {percentage && (
+                <Grid item>
+                  <Chip
+                    variant="combined"
+                    color={isLoss ? 'error' : 'success'}
+                    icon={
+                      <>
+                        {!isLoss && (
+                          <RiseOutlined
+                            style={{ fontSize: '0.75rem', color: 'inherit' }}
+                          />
+                        )}
+                        {isLoss && (
+                          <FallOutlined
+                            style={{ fontSize: '0.75rem', color: 'inherit' }}
+                          />
+                        )}
+                      </>
+                    }
+                    label={`${percentage}%`}
+                    sx={{ ml: 1.25, pl: 1 }}
+                    size="small"
+                  />
+                </Grid>
+              )}
+            </Grid>
             <Typography variant="body1" color="secondary">
               {secondary}
             </Typography>
@@ -37,7 +78,9 @@ ReportCard.propTypes = {
   primary: PropTypes.string,
   secondary: PropTypes.string,
   iconPrimary: PropTypes.func,
-  color: PropTypes.string
+  color: PropTypes.string,
+  percentage: PropTypes.number,
+  isLoss: PropTypes.bool
 };
 
 export default ReportCard;
