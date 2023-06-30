f7c320a991f92d9ff0a0fd5255db2d83c0588a10
Gêdhean Alves <gedhean.dev@gmail.com>
2023-06-30 12:38:44 -0300

    chore: add more data mocks

--- src/data/hives.js
+++ src/data/hives.js
@@ -1,5 +1,15 @@
 // / ==============================|| Mock ||==============================
 
+const defaultCategories = [
+  'Horário 1',
+  'Horário 2',
+  'Horário 3',
+  'Horário 4',
+  'Horário 5',
+  'Horário 6',
+  'Horário 7'
+];
+
 const hives = {
   hive1: {
     id: '1',
@@ -7,15 +17,7 @@ const hives = {
       name: `Apiário 1`,
       data: [25, 30, 31, 32, 33, 29, 26]
     },
-    categories: [
-      'Horário 1',
-      'Horário 2',
-      'Horário 3',
-      'Horário 4',
-      'Horário 5',
-      'Horário 6',
-      'Horário 7'
-    ]
+    categories: defaultCategories
   },
 
   hive2: {
@@ -24,15 +26,7 @@ const hives = {
       name: `Apiário 2`,
       data: [25, 35, 10, 11, 23, 29, 20]
     },
-    categories: [
-      'Horário 1',
-      'Horário 2',
-      'Horário 3',
-      'Horário 4',
-      'Horário 5',
-      'Horário 6',
-      'Horário 7'
-    ]
+    categories: defaultCategories
   },
   hive3: {
     id: '3',
@@ -40,15 +34,15 @@ const hives = {
       name: `Apiário 3`,
       data: [10, 35, 15, 11, 33, 5, 40]
     },
-    categories: [
-      'Horário 1',
-      'Horário 2',
-      'Horário 3',
-      'Horário 4',
-      'Horário 5',
-      'Horário 6',
-      'Horário 7'
-    ]
+    categories: defaultCategories
+  },
+  hive4: {
+    id: '3',
+    series: {
+      name: `Apiário 3`,
+      data: [0, 1, 1, 0, 0, 0, 1]
+    },
+    categories: defaultCategories
   }
 };
 
