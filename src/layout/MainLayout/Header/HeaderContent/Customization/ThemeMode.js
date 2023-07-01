6c73682de983c435ba4726adf6a483bf863021a0
Gêdhean Alves <gedhean.dev@gmail.com>
2023-07-01 10:15:49 -0300

    chore: translate theme mode

--- src/layout/MainLayout/Header/HeaderContent/Customization/ThemeMode.js
+++ src/layout/MainLayout/Header/HeaderContent/Customization/ThemeMode.js
@@ -1,6 +1,17 @@
+// third-party
+import { FormattedMessage } from 'react-intl';
+
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { CardMedia, FormControlLabel, Grid, Radio, RadioGroup, Stack, Typography } from '@mui/material';
+import {
+  CardMedia,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  Stack,
+  Typography
+} from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
@@ -23,22 +34,47 @@ const ThemeModeLayout = () => {
   };
 
   return (
-    <RadioGroup row aria-label="payment-card" name="payment-card" value={mode} onChange={handleModeChange}>
+    <RadioGroup
+      row
+      aria-label="payment-card"
+      name="payment-card"
+      value={mode}
+      onChange={handleModeChange}
+    >
       <Grid container spacing={1.75} sx={{ ml: 0 }}>
         <Grid item>
           <FormControlLabel
             control={<Radio value={ThemeMode.LIGHT} sx={{ display: 'none' }} />}
-            sx={{ display: 'flex', '& .MuiFormControlLabel-label': { flex: 1 } }}
+            sx={{
+              display: 'flex',
+              '& .MuiFormControlLabel-label': { flex: 1 }
+            }}
             label={
               <MainCard
                 content={false}
-                sx={{ bgcolor: mode === ThemeMode.LIGHT ? 'primary.lighter' : 'secondary.lighter', p: 1 }}
+                sx={{
+                  bgcolor:
+                    mode === ThemeMode.LIGHT
+                      ? 'primary.lighter'
+                      : 'secondary.lighter',
+                  p: 1
+                }}
                 border={false}
-                {...(mode === ThemeMode.LIGHT && { boxShadow: true, shadow: theme.customShadows.primary })}
+                {...(mode === ThemeMode.LIGHT && {
+                  boxShadow: true,
+                  shadow: theme.customShadows.primary
+                })}
               >
                 <Stack spacing={1.25} alignItems="center">
-                  <CardMedia component="img" src={defaultLayout} alt="Vertical" sx={{ borderRadius: 1, width: 64, height: 64 }} />
-                  <Typography variant="caption">Claro</Typography>
+                  <CardMedia
+                    component="img"
+                    src={defaultLayout}
+                    alt="Vertical"
+                    sx={{ borderRadius: 1, width: 64, height: 64 }}
+                  />
+                  <Typography variant="caption">
+                    <FormattedMessage id="light" />
+                  </Typography>
                 </Stack>
               </MainCard>
             }
@@ -47,17 +83,36 @@ const ThemeModeLayout = () => {
         <Grid item>
           <FormControlLabel
             control={<Radio value={ThemeMode.DARK} sx={{ display: 'none' }} />}
-            sx={{ display: 'flex', '& .MuiFormControlLabel-label': { flex: 1 } }}
+            sx={{
+              display: 'flex',
+              '& .MuiFormControlLabel-label': { flex: 1 }
+            }}
             label={
               <MainCard
                 content={false}
-                sx={{ bgcolor: mode === ThemeMode.DARK ? 'primary.lighter' : 'secondary.lighter', p: 1 }}
+                sx={{
+                  bgcolor:
+                    mode === ThemeMode.DARK
+                      ? 'primary.lighter'
+                      : 'secondary.lighter',
+                  p: 1
+                }}
                 border={false}
-                {...(mode === ThemeMode.DARK && { boxShadow: true, shadow: theme.customShadows.primary })}
+                {...(mode === ThemeMode.DARK && {
+                  boxShadow: true,
+                  shadow: theme.customShadows.primary
+                })}
               >
                 <Stack spacing={1.25} alignItems="center">
-                  <CardMedia component="img" src={darkLayout} alt="Vertical" sx={{ borderRadius: 1, width: 64, height: 64 }} />
-                  <Typography variant="caption">Escuro</Typography>
+                  <CardMedia
+                    component="img"
+                    src={darkLayout}
+                    alt="Vertical"
+                    sx={{ borderRadius: 1, width: 64, height: 64 }}
+                  />
+                  <Typography variant="caption">
+                    <FormattedMessage id="dark" />
+                  </Typography>
                 </Stack>
               </MainCard>
             }
